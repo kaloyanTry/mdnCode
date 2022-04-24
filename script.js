979658e073459d2243953ca8fd64510995962f0b
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2022-04-24 21:35:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-// Object literals ini JS: Literals represet values in JS
-let sales = "Toyota";
+// 1. Object literals:
+// // Object literals in JS: Literals represet values in JS
+// let sales = "Toyota";
 
-function carTypes(name) {
-  if (name === "Honda") {
-    return name;
-  } else {
-    return `Sorry, we don't sell ${name}.`;
-  }
-}
+// function carTypes(name) {
+//   if (name === "Honda") {
+//     return name;
+//   } else {
+//     return `Sorry, we don't sell ${name}.`;
+//   }
+// }
 
-const car = { myCar: "Saturn", getCar: carTypes("Honda"), special: sales };
+// const car = { myCar: "Saturn", getCar: carTypes("Honda"), special: sales };
 
-console.log(car.myCar);
-console.log(car.getCar);
-console.log(car.special);
+// console.log(car.myCar);
+// console.log(car.getCar);
+// console.log(car.special);
 
-// Use numeric or string literal for the name of a property or nest an boject inside another:
-const carObj = { manyCars: { a: "Saab", b: "Jeep" }, 7: "Mazda" };
-console.log(car.manyCars.b);
-console.log(car[7]);
+// // Use numeric or string literal for the name of a property or nest an boject inside another:
+// const carObj = { manyCars: { a: "Saab", b: "Jeep" }, 7: "Mazda" };
+// console.log(car.manyCars.b);
+// console.log(car[7]);
+//////////////////////////////
+
+// 2. Closures: functions in a function:
+const createPet = function (name, sex) {
+  return {
+    setName: function (newName) {
+      name = newName;
+    },
+
+    getName: function () {
+      return name;
+    },
+
+    getSex: function () {
+      return sex;
+    },
+
+    setSex: function (newSex) {
+      if (
+        typeof newSex === "string" &&
+        (newSex.toLowerCase() === "male" || newSex.toLowerCase() === "female")
+      ) {
+        sex = newSex;
+      }
+    },
+  };
+};
+
+const pet = createPet("Vivie", "male");
+console.log(pet.getName() + " " + pet.getSex()); // Vivie
+
+pet.setName("Oliver");
+pet.setSex("male");
+
+console.log(pet.getSex()); // male
+console.log(pet.getName()); // Oliver
