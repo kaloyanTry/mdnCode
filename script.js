dd24f01acc0c4725f2cc40fb4b34d37d8f006ae9
kaloyanTry <kaloyantryavna@gmail.com>
2022-04-26 21:51:03 +0300

    Added - 2 lines

--- script.js
+++ script.js
@@ -83,8 +83,6 @@ function JSClock() {
 }
 console.log(`The time is exactly ${JSClock()} at the moment.`);
 
-console.log("New line added.");
-console.log("Second line added.");
 // Changing time:
 // const clock = () => {
 //   let time = new Date();
