eea421e047425bfb0bc2cb69130cc312fe97bad4
kaloyanTry <kaloyantryavna@gmail.com>
2022-04-29 16:46:08 +0300

    Arrays added

--- script.js
+++ script.js
@@ -71,17 +71,18 @@
 // console.log(restParams(2, 1, 2, 3)); //Output: [2*1, 2*2, 2*3]=[2,4,6]
 // console.log(restParams(3, 6, 7, 8, 9));
 
-function JSClock() {
-  let time = new Date();
-  let hour = time.getHours();
-  let minute = time.getMinutes();
-  let second = time.getSeconds();
-  let tempTime = "" + (hour > 24 ? hour - 24 : hour);
-  tempTime += (minute < 10 ? ":0" : ":") + minute;
-  tempTime += (second < 10 ? ":0" : ":") + second;
-  return tempTime;
-}
-console.log(`The time is exactly ${JSClock()} at the moment.`);
+///////////////////////////////////////////////////////////////////////////// Numbers and dates /Digital Clock:
+// function JSClock() {
+//   let time = new Date();
+//   let hour = time.getHours();
+//   let minute = time.getMinutes();
+//   let second = time.getSeconds();
+//   let tempTime = "" + (hour > 24 ? hour - 24 : hour);
+//   tempTime += (minute < 10 ? ":0" : ":") + minute;
+//   tempTime += (second < 10 ? ":0" : ":") + second;
+//   return tempTime;
+// }
+// console.log(`The time is exactly ${JSClock()} at the moment.`);
 
 // Changing time:
 // const clock = () => {
@@ -98,3 +99,35 @@ console.log(`The time is exactly ${JSClock()} at the moment.`);
 //   setTimeout(clock, 1000);
 // };
 // clock();
+/////////////////////////////////////////
+// Regular expression:
+// const re = /ab + c/; // or using RegEx object:
+// const reObj = new RegExp("ab + c");
+
+// Arrays: Filling array:
+const arr1 = [];
+arr1.length = 8;
+for (let i = 0; i < arr1.length; i++) {
+  arr1[i] = i + 1;
+}
+console.log(arr1);
+
+const arr2 = [];
+arr2.length = 5;
+arr2.fill(3); // fill the array only with 3
+console.log(arr2);
+
+let arr2D = [];
+arr2D.length = 4;
+let arr2D2 = [];
+arr2D2.length = 4;
+
+let a = new Array(4);
+for (let i = 0; i < 4; i++) {
+  arr2D[i] = new Array(4);
+  for (let j = 0; j < 4; j++) {
+    arr2D[i][j] = "[" + i + ", " + j + "]";
+  }
+}
+
+console.log(arr2D);
